18dd981335ec512083fccfcce0cc18c51d39ac3c
Julien Semaan <jul.semaan@gmail.com>
2015-11-18 13:46:31 -0500

    rework ajax call

--- dist/fingerbank-client.js
+++ dist/fingerbank-client.js
@@ -61,21 +61,31 @@ function FingerbankClient(key){
 // Tests run through node so we need to export it
 if(typeof(requireGlobal) == "function") exports.FingerbankClient = FingerbankClient;
 
-FingerbankClient.prototype.resultFromCurrentUserAgent = function(callback){
+FingerbankClient.prototype.endpointFromCurrentUserAgent = function(callback){
   var self = this;
-  return self.resultFromUserAgent(navigator.userAgent, callback);
+  return self.endpointFromUserAgent(navigator.userAgent, callback);
 }
 
-FingerbankClient.prototype.resultFromUserAgent = function(userAgent, callback){
+FingerbankClient.prototype.endpointFromUserAgent = function(userAgent, callback){
   var self = this;
-  $.get("http://127.0.0.1:3000/api/v1/combinations/interogate",
-    {
+  $.ajax({
+    type:"get",
+    url:"http://127.0.0.1:3000/api/v1/combinations/interogate",
+    data:{
       user_agent: userAgent,
       key: self.key,
     },
-    function(data){
+    success: function(data){
       callback(new Endpoint(data));
+    },
+    error: function(response){
+      if(response.status == 404){
+        callback()
+      }
+      else {
+        throw "Failed with error : "+response.status;
+      }
     }
-  );
+  });
 }
 
